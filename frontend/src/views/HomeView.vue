33744d702cfbc6049de90c9cf345b55fddb30acd
laiquah <cotielaiquah@gmail.com>
2023-08-28 21:36:07 +0200

    home view text still needs to be changed

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,61 +1,61 @@
 <template>
   <div>
     <div class="home">
-      <div class="text">
+      <div class="text" id="text">
         <h1>Power Outage Innovations</h1>
       </div>
-      <div class="text">
-        <h3>"lighting your way through every blackout"</h3>
+      <div class="text" id="text">
+        <h2>"lighting your way through every blackout"</h2>
       </div>
+    </div>
       <div class="featuredProducts">
         <h3>FEATURED PRODUCTS</h3>
         <div class="row">
           <div class="col-4">
-            <img src="" alt="generator" loading="lazy" class="img-fluid">
+            <img src="https://i.postimg.cc/RZTCCvWc/images-8.jpg" alt="generator" loading="lazy" class="img-fluid">
           </div>
           <div class="col-4">
-            <img src="" alt="generator" loading="lazy" class="img-fluid">
+            <img src="https://i.postimg.cc/90wYdx6B/download-7.jpg" alt="generator" loading="lazy" class="img-fluid">
           </div>
           <div class="col-4">
-            <img src="" alt="generator" loading="lazy" class="img-fluid">
+            <img src="https://i.postimg.cc/fbLswmnN/download-9.jpg" alt="generator" loading="lazy" class="img-fluid">
           </div>
         </div>
         <div class="row">
           <div class="col-4">
-            <img src="" alt="inverter" loading="lazy" class="img-fluid">
+            <img src="https://i.postimg.cc/XYJtR36H/download-4.jpg" alt="inverter" loading="lazy" class="img-fluid">
           </div>
           <div class="col-4">
-            <img src="" alt="inverter" loading="lazy" class="img-fluid">
+            <img src="https://i.postimg.cc/KzXV3Mkw/download-5.jpg" alt="inverter" loading="lazy" class="img-fluid">
           </div>
           <div class="col-4">
-            <img src="" alt="inverter" loading="lazy" class="img-fluid">
+            <img src="https://i.postimg.cc/x8p3Hwp8/images-5.jpg" alt="inverter" loading="lazy" class="img-fluid">
           </div>
         </div>
         <div class="row">
           <div class="col-4">
-            <img src="" alt="ups" loading="lazy" class="img-fluid">
+            <img src="https://i.postimg.cc/R0CxVftR/download-1.jpg" alt="ups" loading="lazy" class="img-fluid">
           </div>
           <div class="col-4">
-            <img src="" alt="ups" loading="lazy" class="img-fluid">
+            <img src="https://i.postimg.cc/fLFGKrr2/download-2.jpg" alt="ups" loading="lazy" class="img-fluid">
           </div>
           <div class="col-4">
-            <img src="" alt="ups" loading="lazy" class="img-fluid">
+            <img src="https://i.postimg.cc/SR1JDkh0/download-3.jpg" alt="ups" loading="lazy" class="img-fluid">
           </div>
         </div>
         <div class="row">
           <div class="col-4">
-            <img src="" alt="gas stoves" loading="lazy" class="img-fluid">
+            <img src="https://i.postimg.cc/0ybN5Tqj/images-1.jpg" alt="gas stoves" loading="lazy" class="img-fluid">
           </div>
           <div class="col-4">
-            <img src="" alt="gas stoves" loading="lazy" class="img-fluid">
+            <img src="https://i.postimg.cc/8czSKvpc/images-2.jpg" alt="gas stoves" loading="lazy" class="img-fluid">
           </div>
           <div class="col-4">
-            <img src="" alt="gas stoves" loading="lazy" class="img-fluid">
+            <img src="https://i.postimg.cc/MGpmGKpR/download.jpg" alt="gas stoves" loading="lazy" class="img-fluid">
           </div>
         </div>
+        <button class="btn1" href="/about">ABOUT US</button>
       </div>
-      <button class="btn1" href="/about">ABOUT US</button>
-    </div>
   </div>
 </template>
 
@@ -67,13 +67,38 @@
 
 <style scoped>
 .featuredProducts{
-  background-color: #93B1A6;
+  background-color: #040D12;
+}
+
+#text{
+  font-size: larger;
+  font-weight: bolder;
+}
+
+.img-fluid{
+  margin-bottom: 1rem;
+  aspect-ratio: 1/1;
 }
 
 .btn1{
+  margin-top: 2rem;
+  margin-bottom: 2rem;
   background-color: #93B1A6;
   padding: .4rem;
   border-radius: 1rem;
   width: 10rem;
+  color: white;
+}
+
+.home{
+  background-image: url("https://i.postimg.cc/26ZM96Yh/ehteshamul-haque-adit-w-RUz-D8k-MGo-A-unsplash.jpg");
+  background-attachment: fixed;
+  height: 100vh;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+
+h3{
+  text-shadow: 2px 2px white;
 }
 </style>

cc9682fa9016290c78d641ee388a9b067239b79a
Laiquah <cotielaiquah@gmail.com>
2023-08-28 14:34:52 +0200

    navbar halfway completed

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,6 +1,61 @@
 <template>
   <div>
-
+    <div class="home">
+      <div class="text">
+        <h1>Power Outage Innovations</h1>
+      </div>
+      <div class="text">
+        <h3>"lighting your way through every blackout"</h3>
+      </div>
+      <div class="featuredProducts">
+        <h3>FEATURED PRODUCTS</h3>
+        <div class="row">
+          <div class="col-4">
+            <img src="" alt="generator" loading="lazy" class="img-fluid">
+          </div>
+          <div class="col-4">
+            <img src="" alt="generator" loading="lazy" class="img-fluid">
+          </div>
+          <div class="col-4">
+            <img src="" alt="generator" loading="lazy" class="img-fluid">
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-4">
+            <img src="" alt="inverter" loading="lazy" class="img-fluid">
+          </div>
+          <div class="col-4">
+            <img src="" alt="inverter" loading="lazy" class="img-fluid">
+          </div>
+          <div class="col-4">
+            <img src="" alt="inverter" loading="lazy" class="img-fluid">
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-4">
+            <img src="" alt="ups" loading="lazy" class="img-fluid">
+          </div>
+          <div class="col-4">
+            <img src="" alt="ups" loading="lazy" class="img-fluid">
+          </div>
+          <div class="col-4">
+            <img src="" alt="ups" loading="lazy" class="img-fluid">
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-4">
+            <img src="" alt="gas stoves" loading="lazy" class="img-fluid">
+          </div>
+          <div class="col-4">
+            <img src="" alt="gas stoves" loading="lazy" class="img-fluid">
+          </div>
+          <div class="col-4">
+            <img src="" alt="gas stoves" loading="lazy" class="img-fluid">
+          </div>
+        </div>
+      </div>
+      <button class="btn1" href="/about">ABOUT US</button>
+    </div>
   </div>
 </template>
 
@@ -11,5 +66,14 @@
 </script>
 
 <style scoped>
+.featuredProducts{
+  background-color: #93B1A6;
+}
 
+.btn1{
+  background-color: #93B1A6;
+  padding: .4rem;
+  border-radius: 1rem;
+  width: 10rem;
+}
 </style>

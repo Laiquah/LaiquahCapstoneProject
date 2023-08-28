47aaff5ff7c3f708393cf85fb50aba800c030098
Laiquah <cotielaiquah@gmail.com>
2023-08-28 14:03:29 +0200

    added folderstructure

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,10 +1,21 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div>
+    <Navbar />
+    <router-view />
+    <Footer />
+  </div>
 </template>
+
+<script>
+import Navbar from "./components/NavbarComp.vue";
+import Footer from "./components/FooterComp.vue";
+export default {
+  components: {
+    Navbar,
+    Footer,
+  },
+};
+</script>
 
 <style>
 #app {
--- frontend/src/components/NavbarComp.vue
+++ frontend/src/components/NavbarComp.vue
@@ -1,6 +1,38 @@
 <template>
   <div>
-
+    <nav class="navbar navbar-expand-lg">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#"><img src="" alt="logo" loading="lazy"></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <router-link to="/">home</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link to="/about">about</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link to="/products">products</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link to="/help">FAQs</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link to="/contact">contact</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link to="/admin">admin</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link to="/profile">profile</router-link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   </div>
 </template>
 

903ed3da9a03e1c33a402c08e130565b9b685288
laiquah <cotielaiquah@gmail.com>
2023-08-30 15:16:23 +0200

    help page done

--- frontend/src/views/FAQsView.vue
+++ frontend/src/views/FAQsView.vue
@@ -10,21 +10,21 @@
                 </div>
                 <div class="carousel-inner">
                   <div class="carousel-item active">
-                    <img src="https://i.postimg.cc/66hTRmMX/download-4.png" class="d-block w-100 img-fluid" alt="supergen generator" loading="lazy">
+                    <img src="https://i.postimg.cc/66hTRmMX/download-4.png" class="d-block w-100 img-fluid overlay dd" alt="supergen generator" loading="lazy">
                     <div class="carousel-caption d-none d-md-block">
-                      <p class="paragraph">The SuperGen 2000 Portable Generator is a reliable powerhouse for power outages, impressively fuel-efficient for extended use, and emits low noise levels. Its versatile outlet options allow simultaneous charging of various devices, and its overall reliability, fuel efficiency, and quiet operation make it a recommended solution for backup power needs.</p>
+                      <p class="paragraph">"The SuperGen 2000 Portable Generator is a reliable powerhouse for power outages, impressively fuel-efficient for extended use, and emits low noise levels. Its versatile outlet options allow simultaneous charging of various devices, and its overall reliability, fuel efficiency, and quiet operation make it a recommended solution for backup power needs."</p>
                     </div>
                   </div>
                   <div class="carousel-item">
-                    <img src="https://i.postimg.cc/HLCVb11T/download-11.jpg" class="d-block w-100 img-fluid" alt="SwiftWave Pure Sine Wave Inverter" loading="lazy">
+                    <img src="https://i.postimg.cc/g0d12ZrP/Victron-Phoenix-1200.webp" class="d-block w-100 img-fluid overlay" alt="SwiftWave Pure Sine Wave Inverter" loading="lazy">
                     <div class="carousel-caption d-none d-md-block">
-                      <p class="paragraph">The SwiftWave Pure Sine Wave Inverter is a transformative solution for backup power, converting DC to clean AC power for sensitive electronics. Its pure sine wave output ensures device compatibility, and its solid build quality and safety features provide peace of mind. The inverter is an excellent choice for those seeking reliable and consistent power conversion.</p>
+                      <p class="paragraph">"The SwiftWave Pure Sine Wave Inverter is a transformative solution for backup power, converting DC to clean AC power for sensitive electronics. Its pure sine wave output ensures device compatibility, and its solid build quality and safety features provide peace of mind. The inverter is an excellent choice for those seeking reliable and consistent power conversion."</p>
                     </div>
                   </div>
                   <div class="carousel-item">
-                    <img src="https://i.postimg.cc/mkGs5BVr/download-5.png" class="d-block w-100 img-fluid" alt="FlameMaster Deluxe Gas Stove" loading="lazy">
+                    <img src="https://i.postimg.cc/mkGs5BVr/download-5.png" class="d-block w-100 img-fluid overlay" alt="FlameMaster Deluxe Gas Stove" loading="lazy">
                     <div class="carousel-caption d-none d-md-block">
-                      <p class="paragraph">The FlameMaster Deluxe Gas Stove offers a transformative cooking experience with its sleek design, precise heat distribution, and safety features like flame failure protection. The stove's exceptional performance and thoughtful additions, such as continuous grates, elevate culinary endeavors to new heights. This stove's blend of aesthetics, performance, and safety makes it a remarkable addition to any kitchen, catering to both novice cooks and seasoned chefs.</p>
+                      <p class="paragraph">"The FlameMaster Deluxe Gas Stove offers a transformative cooking experience with its sleek design, precise heat distribution, and safety features like flame failure protection. The stove's exceptional performance and thoughtful additions, such as continuous grates, elevate culinary endeavors to new heights. This stove's blend of aesthetics, performance, and safety makes it a remarkable addition to any kitchen, catering to both novice cooks and seasoned chefs."</p>
                     </div>
                   </div>
                 </div>
@@ -42,7 +42,11 @@
         <center>
             <div class="card">
                 <div class="card-body">
-                  This is some text within a card body.
+                  <p class="question">
+                    Question: What are the different types of electricity generators?
+                  </p>
+                  <br>
+                  Answer: There are several types of generators, including AC Generators (Alternators): Generate alternating current (AC) electricity, DC Generators: Generate direct current (DC) electricity, Synchronous Generators: Produce AC electricity synchronized with the grid frequency and Asynchronous Generators (Induction Generators): Often used in wind turbines and some hydroelectric systems.
                 </div>
               </div>
               <center>
@@ -50,7 +54,11 @@
               </center>
               <div class="card">
                 <div class="card-body">
-                  This is some text within a card body.
+                  <p class="question">
+                    Question: What are the types of electricity inverters?
+                  </p>
+                  <br>
+                  Answer: There are several types of inverters, including Square Wave Inverters: Produce a basic square waveform; not commonly used due to compatibility issues, Modified Sine Wave Inverters: Produce a waveform that approximates a sine wave; suitable for many devices but not all, Pure Sine Wave Inverters: Generate a clean and smooth sine waveform, compatible with almost all devices, Grid-Tie Inverters: Convert DC power from solar panels into AC power for use in homes and feed excess power back into the grid and Microinverters: Used in solar panel installations to convert DC power from individual panels into AC power right at the panel.
                 </div>
               </div>
               <center>
@@ -58,7 +66,11 @@
               </center>
               <div class="card">
                 <div class="card-body">
-                  This is some text within a card body.
+                  <p class="question">
+                    Question: What are the different types of electricity power banks?
+                  </p>
+                  <br>
+                  Answer: There are various types of power banks, including Universal Power Banks: Compatible with a wide range of devices, Solar Power Banks: Can be charged using sunlight through built-in solar panels, High-Capacity Power Banks: Have larger battery capacities for multiple charges, Ultra-Portable Power Banks: Smaller and lighter for easy carrying and Laptop Power Banks: Designed to charge laptops and other high-power devices.
                 </div>
               </div>
               <center>
@@ -66,7 +78,11 @@
               </center>
               <div class="card">
                 <div class="card-body">
-                  This is some text within a card body.
+                  <p class="question">
+                    Question: Do solar panels work during cloudy days or at night?
+                  </p>
+                  <br>
+                  Answer: Solar panels do generate some electricity on cloudy days, although at a reduced efficiency compared to sunny days. They do not generate power at night since they require sunlight to produce electricity.
                 </div>
               </div>
               <center>
@@ -74,7 +90,11 @@
               </center>
               <div class="card">
                 <div class="card-body">
-                  This is some text within a card body.
+                  <p class="question">
+                    Question: Can gas stoves be used during power outages?
+                  </p>
+                  <br>
+                  Answer: Yes, gas stoves can be used during power outages because they don't rely on electricity for cooking. However, if your stove has an electric ignition system, you might need to manually light the burners.
                 </div>
               </div>
         </center>
@@ -94,6 +114,11 @@
 </script>
 
 <style scoped>
+.question{
+  font-weight: bolder;
+  font-size:x-large;
+  text-decoration: underline;
+}
 .btn1{
     margin-top: 2rem;
     font-weight: bold;
@@ -116,7 +141,9 @@ hr {
     border: 2px solid white
   }
 .card{
-    width: 75%;
+    width: 60%;
+    margin-bottom: 3rem;
+    margin-top: 3rem;
 }
 .heading {
     margin-top: 2rem;
@@ -126,29 +153,41 @@ hr {
   }
 
   .carousel-inner {
-    width: 20rem;
+    width: 50rem;
     height: 30rem;
-    opacity: 1rem;
   }
   
   .d-block {
-    width: 20rem;
+    width: 50rem;
     height: 30rem;
   }
   
   .carousel {
     text-align: center;
-    width: 20rem;
+    width: 50rem;
     height: 30rem;
     margin-bottom: 2rem;
   }
   
   .carousel-item {
-    width: 20rem;
+    width: 50rem;
     height: 30rem;
   }
 
   .paragraph{
-    color: black;
+    color: white;
+    font-weight: bolder;
+    text-align: center;
+    margin-top: -17rem;
+  }
+
+  .overlay {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5);
+    opacity: 30%;
   }
 </style>

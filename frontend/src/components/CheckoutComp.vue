200334871c3dfdb419f5a11c5d698fb9ce215fd8
laiquah <cotielaiquah@gmail.com>
2023-09-04 16:12:23 +0200

    added a few

--- frontend/src/components/CheckoutComp.vue
+++ frontend/src/components/CheckoutComp.vue
@@ -8,7 +8,7 @@
                         <th class="heading">PRODUCT NAME</th>
                         <th class="heading">PRODUCT IMAGE</th>
                         <th class="heading">PRODUCT QUANTITY</th>
-                        <th class="heading">PRODUCT PRICE</th>
+                        <th class="heading">PRODUCT PRICE (R)</th>
                     </tr>
                 </thead>
                     <tbody>
@@ -18,12 +18,36 @@
                             <td>{{ product.quantity }}</td>
                             <td>R{{ product.prodPrice }}</td>
                         </tr>
-                        <tr>
-                            <td>TOTAL PRICE:R {{ $store.getters.cartTotal }}</td>
-                        </tr>
                     </tbody>
             </table>
-
+            <center>
+                <hr>
+            </center>
+            <div>
+                <div class="price">TOTAL PRICE:R {{ $store.getters.cartTotal }}</div>
+            </div>
+            <h1 class="headings">CARD DETAILS</h1>
+            <center>
+                <div class="card">
+                    <label>Card Number:</label>
+                    <input>
+                    <br>
+                    <label>Expiration Date:</label>
+                    <div class="row">
+                        <div class="col">
+                            <input class="month">
+                        </div>
+                        <div class="col">
+                            <input class="month">
+                        </div>
+                    </div>
+                    <br>
+                    <label>Security Code:</label>
+                    <input>
+                    <br>
+                </div>
+                <button>Submit</button>
+            </center>
         </div>
     </div>
 </template>
@@ -39,6 +63,36 @@
 </script>
 
 <style scoped>
+label{
+    font-weight: bolder;
+    font-size: 1rem;
+}
+
+.month{
+    width: 10rem;
+}
+.card {
+    width: 30rem;
+    height: 19rem;
+    padding: 2rem;
+    margin-bottom: 2rem;
+    background: #5C8374;
+    box-shadow: #759e8f 0px 2px 4px, #759e8f 0px 7px 13px -3px, #759e8f 0px -3px 0px inset;
+  }
+
+hr {
+    width: 75%;
+    border: 2px solid white
+  }
+
+.price{
+    display: flex;
+    justify-content: space-evenly;
+    border: 2px solid white;
+    background: white;
+    color: black;
+    font-weight: bold;
+}
 .heading {
     margin-top: 2rem;
     margin-bottom: 2rem;
@@ -52,5 +106,20 @@
     color: white;
     text-decoration: underline;
     font-weight: bolder;
+  }
+
+  button {
+    padding: .5rem;
+    width: 15rem;
+    border: 2px solid #759e8f;
+    background-color: #93b1a6;
+    color: white;
+    margin-bottom: 1rem;
+    font-weight: bolder;
+    border-radius: 5rem;
+  }
+  
+  button:hover{
+    background-color: #5C8374;
   }
 </style>
